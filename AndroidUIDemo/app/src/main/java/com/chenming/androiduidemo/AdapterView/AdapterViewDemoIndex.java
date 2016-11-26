88a93626e18ac551c37b202d8989221f40bc712b
chenmingIT <857694178@qq.com>
2016-11-26 23:57:47 +0800

    ListView

---
+++ AndroidUIDemo/app/src/main/java/com/chenming/androiduidemo/AdapterView/AdapterViewDemoIndex.java
@@ -0,0 +1,40 @@
+package com.chenming.androiduidemo.AdapterView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.chenming.androiduidemo.R;
+import com.chenming.androiduidemo.model.DemoInfo;
+import com.chenming.androiduidemo.model.DemoItemClickListener;
+import com.chenming.androiduidemo.model.DemoListAdapter;
+
+public class AdapterViewDemoIndex extends AppCompatActivity {
+
+    ListView listView ;
+    /**
+     *title及其对应要跳转的class
+     */
+    private static final DemoInfo[] DEMOS = {
+            new DemoInfo("ListView 列表",ListViewDemo.class)
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent() ;
+        if(intent.hasExtra("title")){
+            setTitle( intent.getExtras().getString("title") );
+
+        }
+
+
+        listView = (ListView)findViewById(R.id.listView) ;
+        listView.setAdapter(new DemoListAdapter(AdapterViewDemoIndex.this,DEMOS));
+        listView.setOnItemClickListener(new DemoItemClickListener(AdapterViewDemoIndex.this,DEMOS));
+    }
+}
