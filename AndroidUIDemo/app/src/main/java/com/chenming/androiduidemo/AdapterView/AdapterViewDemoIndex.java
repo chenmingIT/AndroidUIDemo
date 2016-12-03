24e7220d46010e0020ac0a98df4fb354fe6f9471
chenmingIT <857694178@qq.com>
2016-12-03 20:38:22 +0800

    完善列表适配器

--- AndroidUIDemo/app/src/main/java/com/chenming/androiduidemo/AdapterView/AdapterViewDemoIndex.java
+++ AndroidUIDemo/app/src/main/java/com/chenming/androiduidemo/AdapterView/AdapterViewDemoIndex.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.chenming.androiduidemo.R;
 import com.chenming.androiduidemo.model.DemoInfo;
@@ -19,8 +20,11 @@ public class AdapterViewDemoIndex extends AppCompatActivity {
     private static final DemoInfo[] DEMOS = {
             new DemoInfo("ListView 列表",ListViewDemo.class),
             new DemoInfo("用Adapter创建ListView",AdapterListViewDemo.class),
-            new DemoInfo("GridView 网络视图",GridViewDemo.class)
-
+            new DemoInfo("GridView 网络视图",GridViewDemo.class),
+            new DemoInfo("ExpandableListView 可展开的列表",ExpandableListViewDemo.class),
+            new DemoInfo("Spinner 可选择列表", SpinnerDemo.class),
+            new DemoInfo("AdapterViewFlipper 自带图片播放", AdapterViewFlipperDemo.class),
+            new DemoInfo("StackViewDemo 图片显示叠加",StackViewDemo.class)
     };
 
     @Override
